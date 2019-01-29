0f92fb0b5c8bde526179b121fe10177fda99f057
Mark Evertz <mark.evertz@gmx.de>
2019-01-29 17:23:28 +0100

    Added logback logging

--- src/main/java/net/blissfish/Application.java
+++ src/main/java/net/blissfish/Application.java
@@ -1,5 +1,9 @@
 package net.blissfish;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+	private static final String HELLO_MSG = "Hello v2.0.0";
+	private static final Logger logger = LoggerFactory
+			.getLogger(Application.class);
+	
+	@PostConstruct
+	public void logSomething() {
+		logger.debug("Sample Debug Message");
+		logger.trace("Sample Trace Message");
+	}
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
     
     @RequestMapping("/hello")
     public String getHello() {
-        return "Hello v2.0.0";
+    	logger.debug("Returning ["+HELLO_MSG+"]");
+        return HELLO_MSG;
     }
     
 }
